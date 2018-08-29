ee2f4ac156aa52f70fa500e73ce96e9c1b581764
Robleds <robleds@robleds.local>
2018-08-29 20:17:21 -0300

    url mock debug control

--- src/resources/urls.js
+++ src/resources/urls.js
@@ -8,17 +8,21 @@ if (DEBUG) {
     url = 'https://apidev.nextel.n3r.com.br'
 }
 
+const nextel_mock = {
+    auth: '/5b691543330000173e32de04',
+    user: '/5b860f8f300000904a7294b0',
+    products: '/5b84d4523000005600728f06',
+    save: '/5b8715ff340000db018b586f',
+};
+
+const nextel_production = {
+    auth:       '/vas/token',
+    user:       '/feppn/getportfolio/{msisdn}',
+    products:   '/feppn/catalogo',
+    save:       '/feppn/newportfolio',
+};
+
 export const remoteApi = url
 export const endpoints = {
-    nextel: {
-        auth: '/5b691543330000173e32de04',
-        user: '/5b860f8f300000904a7294b0',
-        products: '/5b84d4523000005600728f06',
-        // products: '/5b7e044a3300004e444a04c4',
-        // products: '/5b73a8803500009d01531c08',
-        // auth: '/vas/token',
-        // user: '/feppn/getportfolio/{msisdn}',
-        // products: '/feppn/catalogo',
-        save: '/feppn/newportfolio',
-    },
+    nextel: DEBUG ? nextel_mock : nextel_production
 }
